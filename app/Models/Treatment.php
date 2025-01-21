fa4eac6a86e4cfa58b281f7c3cbb2627fb30fc7c
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-01-21 10:58:12 +0700

    first commit

---
+++ app/Models/Treatment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\MoneyCast;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Treatment extends Model
+{
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
+}
